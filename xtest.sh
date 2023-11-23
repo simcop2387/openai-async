bc2376ac2a0f029c36d7a646ed4e0a90f44ea58e
Ryan Voots <simcop2387@simcop2387.info>
2023-11-23 08:56:51 -0500

    Make the script use dzil xtest again

--- xtest.sh
+++ xtest.sh
@@ -6,6 +6,5 @@ export OPENAI_API_KEY="0118 999 881 999 119 725 3"
 export OPENAI_API_BASE="http://openai.general1-model.brainiac.ai.simcop2387.info/v1"
 export I_PROMISE_NOT_TO_SUE_FOR_EXCESSIVE_COSTS="Signed, ryan"
 
-#dzil xtest
+dzil xtest
 
-perl -Ilib xt/03-embeddings.t
